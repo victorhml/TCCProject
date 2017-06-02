c2cdf91e88e7d2b104d8fc387b930b4e761432d5
Victor Lisboa <31203078@mackenzista.com.br>
2017-06-01 21:30:39 -0300

    Comentários

--- Controle/tcc.py
+++ Controle/tcc.py
@@ -1,5 +1,5 @@
 #-*-coding: utf-8 -*-
-#import Libraries
+#Importar bibliotecas
 import RPi.GPIO as GPIO
 import time
 import pyrebase
@@ -10,7 +10,7 @@ import os
 import requests
 import spidev
 
-#Firebase Configuration
+#Configuração do Firebase
 config = {
   "apiKey": "AIzaSyBSQnzFDfZ5fIVvraqGOqOchQNmnnxSmkw",
   "authDomain": "testproject-a8a37.firebaseapp.com",
@@ -20,9 +20,10 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 
-#GPIO Setup
+#Inicialização GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
+
 #Portas
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
@@ -39,7 +40,7 @@ GPIO.setup(15, GPIO.OUT)
 #Presença
 GPIO.setup(38, GPIO.IN)
 
-#Firebase Database Intialization
+#Inicialização do banco de dados do Firebase
 db = firebase.database()
 
 #IP
@@ -48,9 +49,11 @@ s.connect(('google.com', 0))
 ip = s.getsockname()[0]
 s.close
 
+#Streaming da câmera
 os.system('sudo pkill uv4l')
 os.system('sudo uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 360 --framerate 120 --server-option \'--port=9090\' --server-option \'--max-queued-connections=30\' --server-option \'--max-streams=25\' --server-option \'--max-threads=29\'')
 
+#Alarme
 def morsecode():
         #DOT
         GPIO.output(18,GPIO.HIGH)
@@ -94,85 +97,91 @@ def morsecode():
         GPIO.output(18,GPIO.LOW)
         time.sleep(.7)
 
+#Envia o IP
+db.child("ip").set({"value": ip})        
+        
+#Inicializa o SPI
 spi = spidev.SpiDev()
 spi.open(0,0)
 
+#Leitura dos dados do MCP3008
 def ReadChannel(channel):
   adc = spi.xfer2([1,(8+channel)<<4,0])
   data = ((adc[1]&3) << 8) + adc[2]
   return data
 
+#Canais do MCP3008
 gas_channel = 0
 flame_channel = 1
 
-#While loop to run until user kills program
+#Repetição
 while(True):
 
-    #Get IP
-    db.child("ip").set({"value": ip})
-
-    #Get Temperature and Humidity
+    #Temperatura e umidade
     hum, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 22)
     db.child("temperatura").set({"value": "{0:.1f}ºC".format(temp)})
     db.child("umidade").set({"value": "{0:.1f}%".format(hum)})
-    #Get value of LED 
+    
+    #Verifica LED
     ban = db.child("portas").child("banheiro").get()
     coz = db.child("portas").child("cozinha").get()
     qua = db.child("portas").child("quarto").get()
     sala = db.child("portas").child("sala").get()
+    #Verifica alarme
     al = db.child("alarme").get()
-    #Sort through children of LED(we only have one)
+    
     for user in ban.each():
-        #Check value of child(which is 'state')
+      
         if(user.val() == "OFF"):
-            #If value is off, turn LED off
+            #Se valor é OFF, LED apaga
             GPIO.output(11, False)
         else:
-            #If value is not off(implies it's on), turn LED on
+            #Se valor é ON, LED acende
             GPIO.output(11, True)
 
     for user in coz.each():
-        #Check value of child(which is 'state')
+      
         if(user.val() == "OFF"):
-            #If value is off, turn LED off
+            #Se valor é OFF, LED apaga
             GPIO.output(12, False)
         else:
-            #If value is not off(implies it's on), turn LED on
+            #Se valor é ON, LED acende
             GPIO.output(12, True)
         
     for user in qua.each():
-        #Check value of child(which is 'state')
+      
         if(user.val() == "OFF"):
-            #If value is off, turn LED off
+            #Se valor é OFF, LED apaga
             GPIO.output(13, False)
         else:
-            #If value is not off(implies it's on), turn LED on
+            #Se valor é ON, LED acende
             GPIO.output(13, True)
 
     for user in sala.each():
-        #Check value of child(which is 'state')
+      
         if(user.val() == "OFF"):
-            #If value is off, turn LED off
+            #Se valor é OFF, LED apaga
             GPIO.output(16, False)
         else:
-            #If value is not off(implies it's on), turn LED on
+            #Se valor é ON, LED acende
             GPIO.output(16, True)
 
     for user in al.each():
-        #Check value of child(which is 'state')
+      
         if(user.val() == "ON"):
-            #If value is off, turn LED off
+            #Se valor é ON, ativa alarme
             morsecode()
         else:
-            #If value is not off(implies it's on), turn LED on
+            #Se valor é OFF, desativa alarme
             GPIO.output(18, GPIO.HIGH)    
 
+    #Verifica presnça
     if GPIO.input(38) == 1:
         db.child("presenca").set({"state": "ON"})
     elif GPIO.input(38) == 0:
         db.child("presenca").set({"state": "OFF"})
 
-
+    #Envia valor de sensor de gás
     gas_level = ReadChannel(gas_channel)
     if gas_level < 300:
         db.child("gas").set({"nivel": "{}".format(gas_level), "state":"OFF"})
@@ -181,6 +190,7 @@ while(True):
         db.child("gas").set({"nivel": "{}".format(gas_level), "state":"ON"})
         time.sleep(.1)
     
+    #Envia valor de sensor de chamas
     flame_level = 1023-ReadChannel(flame_channel)
     if flame_level < 300:
         db.child("fogo").set({"nivel": "{}".format(flame_level), "state":"OFF"})
